7494693d06b2684fa61b474991577147368df70c
srinath39 <ashwinisrinath2580@gmail.com>
2026-03-20 10:36:51 +0530

    first spring project

---
+++ src/main/java/com/MyPackage/SpringFrameworkProject/SpringFrameworkProjectApplication.java
@@ -0,0 +1,13 @@
+package com.MyPackage.SpringFrameworkProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFrameworkProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFrameworkProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/MyPackage/SpringFrameworkProject/SpringFrameworkProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MyPackage.SpringFrameworkProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFrameworkProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
