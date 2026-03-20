48f21de040ac1a5ef2b4679c52eda7fd42b91c54
srinath39 <ashwinisrinath2580@gmail.com>
2026-03-20 10:40:21 +0530

    checking git

--- src/main/java/com/MyPackage/SpringFrameworkProject/SpringFrameworkProjectApplication.java
+++ src/main/java/com/MyPackage/SpringFrameworkProject/SpringFrameworkProjectApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringFrameworkProjectApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SpringFrameworkProjectApplication.class, args);
+		System.out.println("srinath");
 	}
 
 }
